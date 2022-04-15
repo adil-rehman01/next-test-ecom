17713e70c4843e45eb83f2e42c189b4d496a8e53
adil-rehman01 <adilr.gmit@gmail.com>
2022-04-15 14:21:07 +0500

    main

--- components/dashboard/Products/AddProduct.tsx
+++ components/dashboard/Products/AddProduct.tsx
@@ -31,11 +31,27 @@ const AddProduct = () => {
 					</div>
 					{/* Code block ends */}
 					{/* Code block starts */}
-					<div className='flex flex-col md:mr-16 w-full'>
+					<div className='flex flex-col md:mr-16 w-full col-span-2'>
 						<label htmlFor='email' className='text-black text-sm font-bold leading-tight tracking-normal mb-2'>
-							Name
+							Description
 						</label>
-						<input id='name' className='text-gray-600 focus:outline-none focus:border focus:border-indigo-700 bg-white font-normal h-10 flex items-center pl-3 text-sm border-gray-300 rounded border w-full' placeholder='Name' />
+						<textarea
+							id='name'
+							rows={10}
+							className='text-gray-600 focus:outline-none focus:border focus:border-indigo-700 bg-white font-normal flex items-center p-3 text-sm border-gray-300 rounded border w-full'
+							placeholder='Description . . .'
+						></textarea>
+					</div>
+					<div className='flex flex-col md:mr-16 w-full col-span-2'>
+						<label htmlFor='email' className='text-black text-sm font-bold leading-tight tracking-normal mb-2'>
+							Upload Images
+						</label>
+						<label htmlFor='image' className='w-52 h-52'>
+							<div className='border border-dashed bg-blue-50 border-blue-500 flex flex-wrap w-52 h-52 justify-center items-center rounded cursor-pointer'>
+								<div className='text-blue-600 font-medium'>Upload Image</div>
+							</div>
+							<input type='file' name='image' id='image' className='hidden' />
+						</label>
 					</div>
 					{/* Code block ends */}
 				</div>
