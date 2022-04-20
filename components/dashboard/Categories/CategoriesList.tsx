eab27bf398a482a2b30e1d930e3434f34a1179be
adil-rehman01 <adilr.gmit@gmail.com>
2022-04-20 16:28:14 +0500

    main

--- components/dashboard/Categories/CategoriesList.tsx
+++ components/dashboard/Categories/CategoriesList.tsx
@@ -51,60 +51,37 @@ const CategoriesList = ({ categories }: any) => {
 											</td>
 											<td>
 												<div className='relative px-5 pt-2'>
-													{show == 0 ? (
-														<button className='focus:outline-none' onClick={() => setShow(null)}>
-															<svg xmlns='http://www.w3.org/2000/svg' width={20} height={20} viewBox='0 0 20 20' fill='none'>
-																<path
-																	d='M4.16667 10.8332C4.62691 10.8332 5 10.4601 5 9.99984C5 9.5396 4.62691 9.1665 4.16667 9.1665C3.70643 9.1665 3.33334 9.5396 3.33334 9.99984C3.33334 10.4601 3.70643 10.8332 4.16667 10.8332Z'
-																	stroke='#9CA3AF'
-																	strokeWidth='1.25'
-																	strokeLinecap='round'
-																	strokeLinejoin='round'
-																/>
-																<path
-																	d='M10 10.8332C10.4602 10.8332 10.8333 10.4601 10.8333 9.99984C10.8333 9.5396 10.4602 9.1665 10 9.1665C9.53976 9.1665 9.16666 9.5396 9.16666 9.99984C9.16666 10.4601 9.53976 10.8332 10 10.8332Z'
-																	stroke='#9CA3AF'
-																	strokeWidth='1.25'
-																	strokeLinecap='round'
-																	strokeLinejoin='round'
-																/>
-																<path
-																	d='M15.8333 10.8332C16.2936 10.8332 16.6667 10.4601 16.6667 9.99984C16.6667 9.5396 16.2936 9.1665 15.8333 9.1665C15.3731 9.1665 15 9.5396 15 9.99984C15 10.4601 15.3731 10.8332 15.8333 10.8332Z'
-																	stroke='#9CA3AF'
-																	strokeWidth='1.25'
-																	strokeLinecap='round'
-																	strokeLinejoin='round'
-																/>
-															</svg>
-														</button>
-													) : (
-														<button className='focus:outline-none' onClick={() => setShow(0)}>
-															<svg xmlns='http://www.w3.org/2000/svg' width={20} height={20} viewBox='0 0 20 20' fill='none'>
-																<path
-																	d='M4.16667 10.8332C4.62691 10.8332 5 10.4601 5 9.99984C5 9.5396 4.62691 9.1665 4.16667 9.1665C3.70643 9.1665 3.33334 9.5396 3.33334 9.99984C3.33334 10.4601 3.70643 10.8332 4.16667 10.8332Z'
-																	stroke='#9CA3AF'
-																	strokeWidth='1.25'
-																	strokeLinecap='round'
-																	strokeLinejoin='round'
-																/>
-																<path
-																	d='M10 10.8332C10.4602 10.8332 10.8333 10.4601 10.8333 9.99984C10.8333 9.5396 10.4602 9.1665 10 9.1665C9.53976 9.1665 9.16666 9.5396 9.16666 9.99984C9.16666 10.4601 9.53976 10.8332 10 10.8332Z'
-																	stroke='#9CA3AF'
-																	strokeWidth='1.25'
-																	strokeLinecap='round'
-																	strokeLinejoin='round'
-																/>
-																<path
-																	d='M15.8333 10.8332C16.2936 10.8332 16.6667 10.4601 16.6667 9.99984C16.6667 9.5396 16.2936 9.1665 15.8333 9.1665C15.3731 9.1665 15 9.5396 15 9.99984C15 10.4601 15.3731 10.8332 15.8333 10.8332Z'
-																	stroke='#9CA3AF'
-																	strokeWidth='1.25'
-																	strokeLinecap='round'
-																	strokeLinejoin='round'
-																/>
-															</svg>
-														</button>
-													)}
-													{show == 0 && (
+													<button
+														className='focus:outline-none'
+														onClick={() => {
+															show == null ? setShow(null) : setShow(index);
+														}}
+													>
+														<svg xmlns='http://www.w3.org/2000/svg' width={20} height={20} viewBox='0 0 20 20' fill='none'>
+															<path
+																d='M4.16667 10.8332C4.62691 10.8332 5 10.4601 5 9.99984C5 9.5396 4.62691 9.1665 4.16667 9.1665C3.70643 9.1665 3.33334 9.5396 3.33334 9.99984C3.33334 10.4601 3.70643 10.8332 4.16667 10.8332Z'
+																stroke='#9CA3AF'
+																strokeWidth='1.25'
+																strokeLinecap='round'
+																strokeLinejoin='round'
+															/>
+															<path
+																d='M10 10.8332C10.4602 10.8332 10.8333 10.4601 10.8333 9.99984C10.8333 9.5396 10.4602 9.1665 10 9.1665C9.53976 9.1665 9.16666 9.5396 9.16666 9.99984C9.16666 10.4601 9.53976 10.8332 10 10.8332Z'
+																stroke='#9CA3AF'
+																strokeWidth='1.25'
+																strokeLinecap='round'
+																strokeLinejoin='round'
+															/>
+															<path
+																d='M15.8333 10.8332C16.2936 10.8332 16.6667 10.4601 16.6667 9.99984C16.6667 9.5396 16.2936 9.1665 15.8333 9.1665C15.3731 9.1665 15 9.5396 15 9.99984C15 10.4601 15.3731 10.8332 15.8333 10.8332Z'
+																stroke='#9CA3AF'
+																strokeWidth='1.25'
+																strokeLinecap='round'
+																strokeLinejoin='round'
+															/>
+														</svg>
+													</button>
+													{show == index && (
 														<div className='dropdown-content bg-white shadow absolute z-30 left-0 mr-6 '>
 															<div className='text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white'>
 																<p>Edit</p>
